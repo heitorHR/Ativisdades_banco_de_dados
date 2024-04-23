6291ef05971187498db309be9eeae2300b7a5842
heitorHR <143205211+heitorHR@users.noreply.github.com>
2024-04-23 17:38:06 -0300

    Add files via upload

---
+++ atividade23_4_24.sql
@@ -0,0 +1,34 @@
+/* primeira aula da semana 23/4/24  */
+
+/*-------------------------------------------
+/* O que e join
+/*o que são as informações produtos tabelas de preços ela pemite trezer resultados de tabelas!!
+/* escreva uma consulta para selecionar o nome e preço dos produtos;
+
+
+/* union
+
+
+/* select nome , preco'abaixo de 50' categoria -- ALIAS	
+/* from produtos;
+/* where preco < 50;
+
+
+/* union
+
+
+/* select nome , 
+/* from produtos;
+/* where preco  between '50 and 100';
+
+
+
+  
+
+
+  
+  
+
+
+
+                                                                                                                                                                         
